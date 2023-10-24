930e6bdb6be581d2c0aacd3613eb73014103a5db
Hans (cgt103529) <cgt103529@stud.fh-wedel.de>
2023-10-25 01:30:02 +0200

    SimonSays extended, tp might work?

--- EscapeRoomVR/Assets/Scripts/SimonSays.cs
+++ EscapeRoomVR/Assets/Scripts/SimonSays.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SimonSays : MonoBehaviour
 {
     [SerializeField] private GameObject[] buttons;
-
+    [SerializeField] private TextMeshPro textBox;
     private int[] buttonsToHit = new int[5];
     int _counter = 0;
+
+    private int _buttonInputCounter = 0;
+
+    private int[] _buttonInput = new int [5];
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +27,23 @@ public class SimonSays : MonoBehaviour
 
     public void ButtonPressed(int id)
     {
-        Debug.Log(id);
+        if (_buttonInputCounter < 5)
+        {
+            _buttonInput[_buttonInputCounter] = id;
+            _buttonInputCounter++;
+        }
+        else
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                textBox.text += buttonsToHit[i];
+            }
+            textBox.text += "/n";
+            for (int i = 0; i < 5; i++)
+            {
+                textBox.text += _buttonInput[i];
+            }
+        }
     }
 
     IEnumerator TurnColorButton(int [] i)
@@ -28,7 +51,7 @@ public class SimonSays : MonoBehaviour
         Renderer buttonRenderer = buttons[i[_counter]].GetComponent<Renderer>();
         buttonRenderer.material.SetColor("_Color",Color.green);
         yield return new WaitForSeconds(1f);
-        buttonRenderer.material.SetColor("_Color",Color.white);
+        buttonRenderer.material.SetColor("_Color",Color.red);
         _counter++;
         if (_counter < 5)
         {
